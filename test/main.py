d207d776c000a66ad98a79851323558a3c5a927b
yuliteng9 <li104.li@gmail.com>
2021-07-16 06:40:58 -0400

    Show total confirmed cases Create a function which can take a country name to show total confirmed cases in that country

--- test/main.py
+++ test/main.py
@@ -8,13 +8,22 @@ window = tk.Tk()
 window.geometry("800x400")
 window.title("Covid Stats")
 
-for index in response.json()["Countries"]:
+def show_data(country_name):
+    for index in response.json()["Countries"]:
     #response.json() is a dictionary, response.json()["Countries"] is the value of "Countries", which is a long list
     #this list consists of dictionaries, each dictionary contains the covid info of a country
     #index is the item in this list, so index is a dictionary
-    if index["Country"] == "Canada":
-        print(index) #print out the whole dictionary, in which contains the info of Canada
+        if index["Country"] == country_name:
+        #print(index) #to print out the whole dictionary, in which contains the info of Canada
+            country_label = tk.Label(window, font="Ariel 28", text=country_name)
+            country_label.pack()
+        #confirmed_description = tk.Label(window, text="Total confirmed covid cases")
+        #confirmed_description.pack()
+            total_confirmed = index["TotalConfirmed"]
+            confirmed_label = tk.Label(window, font="Ariel 10", text="Total confirmed covid cases: " + str(total_confirmed))
+            confirmed_label.pack()
 
+show_data("Canada")
 
 window.mainloop()
 
